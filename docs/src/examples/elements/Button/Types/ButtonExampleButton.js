e2684e634d9537c0006b5cf1101e2849739a2895
yeog <39256621+yeog@users.noreply.github.com>
2018-08-02 08:12:49 +0900

    docs(ButtonExampleButton): your description

--- docs/src/examples/elements/Button/Types/ButtonExampleButton.js
+++ docs/src/examples/elements/Button/Types/ButtonExampleButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-const ButtonExampleButton = () => <Button>Click Here</Button>
+const ButtonExampleButton = () => <Button>Click here here here here</Button>
 
 export default ButtonExampleButton
